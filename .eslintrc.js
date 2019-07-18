6e4bfd13200c85a5a90e0c6ad0517d50c60e56ae
sheldon <websheldoncui@gmail.com>
2019-07-18 21:59:13 +0800

    chore: 增加eslint prettier commitlint 配置

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true, // 开启浏览器全局变量
+    node: true, // 开启 node 全局变量
+    es6: true, // 开启 es6 全局变量 如 Set
+    jquery: true, // jquery 全局变量
+  },
+  extends: [
+    'plugin:vue/recommended',
+    'standard',
+    'plugin:prettier/recommended',
+  ], // 因为我们要用prettier风格规范替代eslint的，所以prettier要放在最后
+  rules: {
+    semi: ['error', 'never'],
+  },
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+  },
+};
